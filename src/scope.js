13afb38d8f1a60b8ae051b40c70ec79d2ff9c53d
Jim Rottinger <jamesrottinger@gmail.com>
2018-03-18 14:41:32 -0700

    Add $watchCollection function

--- src/scope.js
+++ src/scope.js
@@ -2,6 +2,15 @@
 
 var _ = require('lodash');
 
+function isArrayLike(obj) {
+  if (_.isNull(obj) || _.isUndefined(obj)) {
+    return false;
+  }
+  var length = obj.length;
+  return length === 0 ||
+    (_.isNumber(length) && length > 0 && (length - 1) in obj);
+}
+
 function Scope() {
   this.$$watchers = [];
   this.$$lastDirtyWatcher = null;
@@ -104,6 +113,90 @@ function Scope() {
         fn();
       });
     };
+  };
+
+  this.$watchCollection = function(watchFn, listenerFn) {
+    var newValue, oldValue, oldLength, veryOldValue;
+    var trackVeryOldValue = (listenerFn.length > 1);
+    var changeCount = 0;
+    var firstRun = true;
+
+    var internalWatchFn = function(scope) {
+      var newLength;
+
+      newValue = watchFn(scope);
+      if (_.isObject(newValue)) {
+        if (isArrayLike(newValue)) {
+          if (!_.isArray(oldValue)) {
+            changeCount++;
+            oldValue = [];
+          }
+          if (newValue.length !== oldValue.length) {
+            changeCount++;
+            oldValue.length = newValue.length;
+          }
+          _.forEach(newValue, function(newItem, i) {
+            var bothNaN = _.isNaN(newItem) && _.isNaN(oldValue[i]);
+            if (!bothNaN && newItem !== oldValue[i]) {
+              changeCount++;
+              oldValue[i] = newItem;
+            }
+          });
+        } else {
+          if (!_.isObject(oldValue) || isArrayLike(oldValue)) {
+            changeCount++;
+            oldValue = {};
+            oldLength = 0;
+          }
+          newLength = 0;
+          _.forOwn(newValue, function(newVal, key) {
+            newLength++;
+            if (oldValue.hasOwnProperty(key)) {
+              var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+              if (!bothNaN && oldValue[key] !== newVal) {
+                changeCount++;
+                oldValue[key] = newVal;
+              }
+            } else {
+              changeCount++;
+              oldLength++;
+              oldValue[key] = newVal;
+            }
+          });
+          if (oldLength > newLength) {
+            changeCount++;
+            _.forOwn(oldValue, function(oldVal, key) {
+              if (!newValue.hasOwnProperty(key)) {
+                oldLength--;
+                delete oldValue[key];
+              }
+            });
+          }
+        }
+      } else {
+        if (!this.$$areEqual(newValue, oldValue, false)) {
+          changeCount++;
+        }
+        oldValue = newValue;
+      }
+
+      return changeCount;
+    }.bind(this);
+
+    var internalListenerFn = function() {
+      if (firstRun) {
+        listenerFn(newValue, newValue, this);
+        firstRun = false;
+      } else {
+        listenerFn(newValue, veryOldValue, this);
+      }
+
+      if (trackVeryOldValue) {
+        veryOldValue = _.clone(newValue);
+      }
+    }.bind(this);
+
+    return this.$watch(internalWatchFn, internalListenerFn);
   };
 
   this.$beginPhase = function(phase) {
